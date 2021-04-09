4acdfd4f3e85fa1e0af867a808cb6251a3308263
pixcompu <fadm.contacto@gmail.com>
2021-04-09 02:06:30 -0500

    improved voice channel example, now it works

--- app/commands/Music.php
+++ app/commands/Music.php
@@ -7,26 +7,63 @@ use Discord\Voice\VoiceClient;
 class Music extends Command
 {
 
+	/**
+	 * play a static music file if the user that triggered the command is in a voice channel
+	 * @throws \Exception
+	 */
 	public function execute(): void
 	{
-		$voiceStates = $this->message->channel->guild->voice_states;
-		$userId = $this->message->author->id;
+		// get the guild where the bot was called from the db
+		$guild = $this->discord->guilds->first();
 
+		// to find the voice channel the user is connected
+		// we need to find all the voice states from the guils (voice states are combinations of voice channels and users)
+		$voiceStates = $guild->voice_states;
+
+		// once we have the voice states we need to find the voice state the user is related to
+		// a user only can be on a single voice channel at the same time
 		$channel = null;
-		foreach ($voiceStates as $voiceState) { //Find a voice channel the user is in
-			if ($voiceState->user_id === $userId) {
+		foreach ($voiceStates as $voiceState)
+		{
+			// found the user who send the command, then this channel must be the voice channel the user
+			if($voiceState->user_id === $this->message->user_id){
 				$channel = $this->discord->getChannel($voiceState->channel_id);
+				break;
 			}
 		}
 
+		// if we found successfully the channel then we attempt to join the voice channel
 		if (isset($channel)) {
-			$this->discord->joinVoiceChannel($channel)->then(function (VoiceClient $voice) {
-				$voice->playFile(__DIR__.'./../../test.mp3')->otherwise(function($o){
-					echo json_encode($o);
-				})->then([$voice, 'close']);
+			$this->discord->joinVoiceChannel($channel)->then(function(VoiceClient $voiceClient){
+				$this->message->reply(tt('command.music.player_start'));
+				$this->play($voiceClient);
 			});
+			// the user that triggered the command isn't on a voice channel
 		} else {
 			$this->message->reply(tt('command.music.invalid_channel'));
 		}
+	}
+
+	/**
+	 * Play a static file on the voice channel
+	 * @param VoiceClient $voiceClient
+	 */
+	private function play(VoiceClient $voiceClient)
+	{
+		// play raw sound file
+		$voiceClient->playFile(__DIR__.'/../../test.mp3')
+			->then(function() use ($voiceClient){
+				// this is executed when the music stopped playing
+				$voiceClient->close()->then(function(){
+					// successfully played the file and abandoned the voice channel
+					$this->message->reply(tt('command.music.player_end'));
+				}, function(){
+					// error while abandoning the voice channel
+					$this->message->reply(tt('command.music.player_end_error'));
+				});
+			}, function(){
+				// error while playing the file
+				$this->message->reply(tt('command.music.player_start_error'));
+			});
 	}
 }
