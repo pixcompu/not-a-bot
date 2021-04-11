723317f43af70ac81ca60c246a5de9121d849b81
pixcompu <fadm.contacto@gmail.com>
2021-04-11 13:50:24 -0500

    improve top command to expand the limit of messages

--- app/commands/Top.php
+++ app/commands/Top.php
@@ -3,13 +3,26 @@
 namespace app\commands;
 
 use Discord\Helpers\Collection;
+use Discord\Parts\Channel\Message;
+use Google\CRC32\PHP;
 use util\Text;
 
 class Top extends Command
 {
+	/**
+	 * @var array
+	 */
+	private array $allMessages = [];
+
+	/**
+	 * we don't want to get a infinite load of messages, so we set a sanity check on the limit of messages
+	 * @var int
+	 */
+	private int $max = 5000;
 
 	/**
 	 * gives the most sent messages in a channel (based on the last 100 messages)
+	 * @throws \Exception
 	 */
 	public function execute(): void
 	{
@@ -19,24 +32,23 @@ class Top extends Command
 		}
 
 		// get the guild where the bot was called from the db
-		$this->message->channel->getMessageHistory([
-			'before' => $this->message
-		])->then(function (Collection $messages) use($limit){
-			$this->showTopMessages($messages, $limit);
+		$this->retrieveFullMessageHistory($this->message, function()use ($limit){
+			$this->showTopMessages($limit);
+		}, function(){
+			$this->message->reply(tt('command.top.error'));
 		});
 	}
 
 	/**
 	 * calculates and shows the list of most sent messages in the channel
-	 * @param Collection $messages
 	 * @param int $limit
 	 * @return void
 	 * @throws \Exception
 	 */
-	private function showTopMessages(Collection $messages, int $limit) : void
+	private function showTopMessages(int $limit) : void
 	{
 		// first we will get a list of raw messages so we can use PHP built in functions to count
-		$countByMessage = array_reduce($messages->toArray(), function ($map, $message) {
+		$countByMessage = array_reduce($this->allMessages, function ($map, $message) {
 			// if out bot or another bot is the author of the message, discard it
 			$isBot = $message->author->bot || $this->botDiscord->user->id === $message->user_id;
 			// if the message it's used to chat with another person (contains mentions), discard it
@@ -71,7 +83,7 @@ class Top extends Command
 
 		// process each message in the count relation
 		$lines = [];
-		$lines[] = $this->bold(tt('command.top.start') . ':');
+		$lines[] = $this->bold(sprintf(tt('command.top.start'), count($this->allMessages)) . ':');
 		// calculate if the actual results are less than the limit requested, then return only as much results as we can calculate
 		// otherwise return the same number of elements the limit is requesting
 		$min = min($limit, count($messageCounts));
@@ -99,5 +111,38 @@ class Top extends Command
 			// otherwise reply
 			$this->message->reply(tt('command.top.end_empty'));
 		}
+	}
+
+	/**
+	 * Recursive function to get the full message history of a channel
+	 * @param Message $message
+	 * @param callable $onDone
+	 * @param callable $onFail
+	 */
+	private function retrieveFullMessageHistory(Message $message, callable $onDone, callable $onFail)
+	{
+		// get the last 100 messages before the message passed in
+		$this->message->channel->getMessageHistory([
+			'before' => $message,
+			'limit' => 100
+		])->then(function (Collection $messages) use($onDone, $onFail){
+			// when we get the history of messages we will evaluate if we neeed to make another call to get more messages
+			// or we reached the end of the channel history
+			$rawMessages = array_values($messages->toArray());
+
+			// sort by date asc
+			usort($rawMessages, function($a, $b){
+				if($a['timestamp'] === $b['timestamp']) return 0;
+				return strtotime($a['timestamp']) - strtotime($b['timestamp']);
+			});
+
+			$this->allMessages = array_merge($rawMessages, $this->allMessages);
+			// we either reached the end of the channel or reached our max limit of messages we want to retrieve
+			if(count($rawMessages) < 100 || count($this->allMessages) >= $this->max){
+				call_user_func($onDone);
+			} else {
+				$this->retrieveFullMessageHistory($rawMessages[0], $onDone, $onFail);
+			}
+		}, $onFail);
 	}
 }
